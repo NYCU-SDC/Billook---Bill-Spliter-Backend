1dbd0a0259614000e3120fafe04e615b3a66cace
r1407p <buszter0403@gmail.com>
2024-04-24 00:19:32 +0800

    feat: init  the backend

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+import { print, buildSchema } from 'graphql';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+// Create an express app
+const app = express();
+const port = 5000;
+
+
+app.use(bodyParser.json());
+
+
+const MONGO_URI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port localhost:${port}`);
+            console.log(`Visit http://localhost:${port}/graphql to interact with the GraphQL API.`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
