569a57ca5571569bf3367730386b13fd6ba059e9
r1407p <buszter0403@gmail.com>
2024-04-24 02:14:04 +0800

    feat: finish basic graphql with user

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import { graphqlHTTP } from 'express-graphql';
 import mongoose from 'mongoose';
 import { print, buildSchema } from 'graphql';
 import dotenv from 'dotenv';
-import User from './models/user.js';
+import graphqlTypedef from './graphql/schema/index.js';
+import graphqlResolvers from './graphql/resolvers/index.js';
 
 
 dotenv.config();
@@ -18,24 +19,41 @@ app.use(bodyParser.json());
 
 
 const MONGO_URI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-app.get('/get_all_user',async (req, res) => {
-    try{
-        const users = await User.find();
-        res.json(users);
-    }catch(err){
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-
+const graphqlSchema = buildSchema(print(graphqlTypedef));
+    console.log(print(graphqlTypedef));
+    app.use('/graphql', graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolvers,
+        graphiql: true, // Enable GraphiQL when accessed via browser
+      }));
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+//   });
+// app.get('/get_all_user',async (req, res) => {
+//     try{
+//         const users = await User.find();
+//         res.json(users);
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).json({ message: 'Internal Server Error' });
+//     }
+//   });
+// app.post('/create_user', async (req, res) => {
+//     try {
+//         const { username, password, email } = req.body;
+//         const newUser = new User({ username, password, email });
+//         await newUser.save();
+//         res.status(201).json(newUser);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ message: 'Internal Server Error' });
+//     }
+// });
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(port, () => {
             console.log(`Server is running on port localhost:${port}`);
-            console.log(`Visit http://localhost:${port}/`);
+            console.log(`Visit http://localhost:${port}/graphql to interact with the GraphQL API.`);
         });
     })
     .catch(err => {
---
+++ graphql/resolvers/transformer.js
@@ -0,0 +1,34 @@
+import models from '../../models/index.js';
+// import dateToString from  '../../helpers/date.js';
+
+
+const transformUser = user => {
+    return {
+        ...user._doc,
+        _id: user.id,
+        // createdBooks: getListTransformedBooks.bind(this, user.createdBooks)
+    };
+}
+
+const getSingleTransformedUser = async userID => {
+    try {
+        const user = await models.Users.findById(userID);
+        return transformUser(user);
+    } catch (err) {
+        throw err;
+    }
+}
+
+const getListTransformedUser = async userIDs => {
+    try {
+        const users = await models.Users.find({ _id: { $in: userIDs } });
+        return users.map(user => {
+            return transformUser(user);
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+
+export { transformUser, getSingleTransformedUser, getListTransformedUser };
